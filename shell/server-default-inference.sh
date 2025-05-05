0f1afb0899d374f04af7d3c06b5c64e5546dd039
Ectisia <ncklact@gmail.com>
2025-05-05 21:28:52 +0800

    modified sh

--- shell/server-default-inference.sh
+++ shell/server-default-inference.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# This script is used to run the inference server with default settings.
+
+export HF_ENDPOINT=https://hf-mirror.com
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+
 python ./api_server.py \
 -i \
 -p 8001
--- shell/server-large-inference.sh
+++ shell/server-large-inference.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# This script is used to run the inference server with large model.
+
+export HF_ENDPOINT=https://hf-mirror.com
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+
 python ./api_server.py \
 -i \
 -p 8001 \
